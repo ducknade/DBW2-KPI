a9181e0058b4afb130f3d8eb130b1f70c120564f
HummingTree <jt2798@columbia.edu>
2019-03-24 19:11:10 -0400

    finilize fft.py

--- conf.sh
+++ conf.sh
@@ -6,44 +6,44 @@
 bc_name="periodic"
 
 # ensemble name
-ens_name="periodic-10x20"
+ens_name="L32_beta6.4_4.0_5"
 
 # directory of the wilson flow files
-ens_dir="/Users/hummingtree/alice/DBW2-KPI/wflow_tcharge_cps/results/periodic-10x20"
+ens_dir="/Users/hummingtree/alice/DBW2-KPI/wflow_tcharge_cps/results/$ens_name"
 
 # number of thermalization steps, i.e. skip these configurations
-num_thermal="300"
+num_thermal="3000"
 
 # time extent size
-T="20"
+T="32"
 
 # MD spacing between two consecutive measurements
-meas_spacing_MD="15"
+meas_spacing_MD="10"
 
 # block size in MD unit
-block_size_MD="1500"
+block_size_MD="1000"
 
 # the maximum MD separation when computing correlations
 #max_MD_sep=$[$block_size_MD]
-max_MD_sep="750"
+max_MD_sep="500"
 #echo $max_MD_sep
 
 # the maximum MD separation when computing error of the correlations
 # err_max_MD_sep=$[$block_size_MD/4]
-err_max_MD_sep=300
+err_max_MD_sep=250
 #echo $err_max_MD_sep
 
 # number of replicas
 num_reps="1"
 
 # list of replicas
-rep_names="periodic-10x20"
+rep_names="L32_beta6.4_4.0_5"
 
 # initial values of the parameters for the fit
 tau_guess="12.13"
 D_guess="0.0635"
 
 wfs_ref_min="0.0"
-wfs_ref_max="0.25"
+wfs_ref_max="0.30"
 wfs_ref_add="0.05"
 
--- fft/fft.py
+++ fft/fft.py
@@ -104,41 +104,59 @@ dtau = 5
 fit_min = 0
 fit_max = 6
 
-def generate_gnuplot_script(ens, dt):
-  with open("%s_adaptive.gnuplot" %ens, 'w') as f:
-    print("set terminal epslatex size 16.00cm, 8.00cm color lw 2 standalone colortext font 10 \\", file=f)
+def generate_gnuplot_script(ens, dt, num_col, diff=False):
+  script_path="plots/%s_adaptive.gnuplot" %ens
+  if diff:
+    script_path="plots/%s_difference.gnuplot" %ens
+  with open(script_path, 'w') as f:
+    print("set terminal epslatex size 20.00cm, 10.00cm color lw 2 standalone colortext font 10 \\", file=f)
     print("	header '\usepackage{grffile} \usepackage{amssymb}'", file=f)
     print("", file=f)
     print("set xtics", file=f)
     print("set ytics", file=f)
-    print("set ylabel '$\log[C(k,0)/C(k,%d)]/%d$'" %(dt, dt), file=f)
+    if diff:
+      print("set ylabel '$\log[C(k,0)/C(k,%d)]/%d-\log[C(k,%d)/C(k,%d\cdot2)]/%d$'" %(dt, dt, dt, dt, dt), file=f)
+    else:
+      print("set ylabel '$\log[C(k,0)/C(k,%d)]/%d$'" %(dt, dt), file=f)
     print("set xlabel '$2-2\cos k$'", file=f)
     print("", file=f)
     print("set xrange [-0.1:5.8]", file=f)
-    print("set yrange [-0.05:0.85]", file=f)
+    if diff:
+      print("set yrange [-0.25:0.65]", file=f)
+    else:
+      print("set yrange [-0.05:0.85]", file=f)
     print("", file=f)
     print("set bars small", file=f)
     print("", file=f)
-    print("outfile = '%s_adaptive'    # Also used for filename generation so set it" %ens, file=f)
+    if diff:
+      print("outfile = 'plots/%s_difference'    # Also used for filename generation so set it" %ens, file=f)
+    else:
+      print("outfile = 'plots/%s_adaptive'    # Also used for filename generation so set it" %ens, file=f)
     print("", file=f)
-    print("set key spacing 1.2", file=f)
+    print("set key spacing 1.4", file=f)
     print("set key enhanced", file=f)
     print("set key top right", file=f)
     print("", file=f)
     print("set output outfile.'.tex'", file=f)
     print("", file=f)
     print("two_pi = 6.28318530718", file=f)
+    print("two_pi = 6.28318530718", file=f)
     print("", file=f)
-    print("infile = '%s_adaptive.dat'" %ens, file=f)
+    if diff:
+      print("set label 1 at graph 0.1, graph 0.9 '\\texttt{%s diff.}' " %ens.replace("_","\\_"), file=f)
+      print("infile = '%s_difference.dat'" %ens, file=f)
+    else:
+      print("set label 1 at graph 0.1, graph 0.9 '\\texttt{%s value}' " %ens.replace("_","\\_"), file=f)
+      print("infile = '%s_adaptive.dat'" %ens, file=f)
     print("", file=f)
-    print("plot for [i=1:7:1] infile using (2-2*cos($1*two_pi)+0.04*i):(column(2*i)):(column(2*i+1)) with ye lw 1.5 pt 7 title columnheader(i*2), \\", file=f)
+    print("plot for [i=1:%d:1] infile using (2-2*cos($1*two_pi)+0.04*i):(column(2*i)):(column(2*i+1)) with ye lw 1.5 pt 7 title columnheader(i*2), \\" %num_col, file=f)
     print("", file=f)
     print("set output # Need to close the output file before latex can work with it", file=f)
     print("", file=f)
-    print("system 'pdflatex -shell-escape -interaction=batchmode '.outfile.\\", file=f)
+    print("system 'pdflatex -shell-escape -interaction=batchmode -output-directory=plots '.outfile.\\", file=f)
     print("	'.tex > /dev/null; rm '.outfile.'-inc.eps '.outfile.\\", file=f)
-    print("		'-inc-eps-converted-to.pdf '.outfile.'.tex $(basename '.\\", file=f)
-    print("			outfile.').log $(basename '.outfile.').aux'\\", file=f)
+    print("		'-inc-eps-converted-to.pdf '.outfile.'.tex '.\\", file=f)
+    print("			outfile.'.log '.outfile.'.aux'\\", file=f)
     print("", file=f)
     print("system 'open '.outfile.'.pdf'", file=f)
 
@@ -175,7 +193,7 @@ def cv_err_jackknife(data):
   err = np.sqrt(n+1)*std(data[1:n+1])
   return [cv, err]
 
-def compute(filename_stem, jack, T, tau_limit):
+def compute(filename_stem, jack, T, tau_limit, diff):
 
   data     = []
   for tau in range(tau_limit):
@@ -191,51 +209,56 @@ def compute(filename_stem, jack, T, tau_limit):
         data[tau][dt] = float( lines[tau].split()[1] )
   fft_data = [ np.fft.fft( data[tau] ) for tau in range(tau_limit) ]
 
-  return [ 0.5*np.log((fft_data[0+0][n].real/fft_data[0+1][n].real)**2) for n in range(T)]
+  if diff:
+    return [ 0.5*np.log(fft_data[0+0][n].real*fft_data[0+2][n].real/(fft_data[0+1][n].real)**2) if fft_data[0+0][n].real*fft_data[0+2][n].real>0. else np.nan for n in range(T)]
+  else:
+    return [ np.log(fft_data[0+0][n].real/fft_data[0+1][n].real) if fft_data[0+0][n].real/fft_data[0+1][n].real>0. else np.nan for n in range(T)]
 
-def compute_ens(ens_name, Njack, T, tau_limit, dtau, fit_min, fit_max, label):
+def compute_ens(ens_name, Njack, T, tau_limit, dtau, fit_min, fit_max, label, diff):
   filename_stem = "../Qslice_to_correlation/%s/correlations_jackknife" % (ens_name)
   x = [ 2.-2.*np.cos(k*2.*math.pi) for k in np.fft.fftfreq(T)]
 
-  D = []
-  t = []
+#  D = []
+#  t = []
   lst = []
   for jack in range(Njack):
-    jlst = compute(filename_stem, jack, T, tau_limit)
+    jlst = compute(filename_stem, jack, T, tau_limit, diff)
     lst.append( [y/float(dtau) for y in jlst] )
-    y = [ B for B in lst[jack] ]
-    pol = np.polyfit(x[fit_min:fit_max],y[fit_min:fit_max],1)
-    D.append(pol[0])
-    t.append(1./pol[1])
-  
-  f = open(label, 'w')
-  
-  f.write( "D: %12.8e %12.8e\n" % ( D[0], np.sqrt(Njack+1)*std( D[1:Njack] ) ) )
-  f.write( "t: %12.8e %12.8e\n" % ( t[0], np.sqrt(Njack+1)*std( t[1:Njack] ) ) )
-  f.write( "[]: %.4f %.4f\n" % (x[fit_min], x[fit_max-1]) )
-  print("%s & %s & %s \\\\" %( label, format_number_with_err(D[0], np.sqrt(Njack+1)*std( D[1:Njack])), format_number_with_err(t[0], np.sqrt(Njack)*std( t[1:Njack])) ) ) 
+#    y = [ B for B in lst[jack] ]
+#    pol = np.polyfit(x[fit_min:fit_max],y[fit_min:fit_max],1)
+#    D.append(pol[0])
+#    t.append(1./pol[1])
+#  
+#  f = open(label, 'w')
+#  
+#  f.write( "D: %12.8e %12.8e\n" % ( D[0], np.sqrt(Njack+1)*std( D[1:Njack] ) ) )
+#  f.write( "t: %12.8e %12.8e\n" % ( t[0], np.sqrt(Njack+1)*std( t[1:Njack] ) ) )
+#  f.write( "[]: %.4f %.4f\n" % (x[fit_min], x[fit_max-1]) )
+#  print("%s & %s & %s \\\\" %( label, format_number_with_err(D[0], np.sqrt(Njack+1)*std( D[1:Njack])), format_number_with_err(t[0], np.sqrt(Njack)*std( t[1:Njack])) ) ) 
 #  print "D: %12.8e %12.8e" % ( D[0], np.sqrt(Njack+1)*std( D[1:Njack] ) ) 
 #  print "t: %12.8e %12.8e" % ( t[0], np.sqrt(Njack+1)*std( t[1:Njack] ) ) 
 
-  for n in range(T):
-    f.write("%+8.4e %12.8e %12.8e\n" % ( np.fft.fftfreq(T)[n], lst[0][n], np.sqrt(Njack)*std([lst[j][n] for j in range(1,Njack,1)]) ) )
+#  for n in range(T):
+#    f.write("%+8.4e %12.8e %12.8e\n" % ( np.fft.fftfreq(T)[n], lst[0][n], np.sqrt(Njack)*std([lst[j][n] for j in range(1,Njack,1)]) ) )
   return [ cv_err_jackknife([lst[j][n] for j in range(Njack)]) for n in range(T)]
 
 
-def compute_ens_wfs(ens_name, Njack, T, tau_limit, dtau, fit_min, fit_max, label):
+def compute_ens_wfs(ens_name, Njack, T, tau_limit, dtau, fit_min, fit_max, label, diff=False):
   fft = []
+  num_col = 0
   for dir_path in os.listdir('../Qslice_to_correlation'):
     if fnmatch.fnmatch(dir_path, "%s_wfs*"%(ens_name)):
       # print dir_path 
       wfs = float(dir_path.split("wfs")[1])
       # print wfs
-      fft.append([wfs, compute_ens(dir_path, Njack+1, T, tau_limit, dtau, fit_min, fit_max, label)])
+      fft.append([wfs, compute_ens(dir_path, Njack+1, T, tau_limit, dtau, fit_min, fit_max, label, diff)])
+      num_col = num_col + 1
   f = open(label, 'w')
   # print fft
   f.write("wfs     ")
   for w in range(len(fft)):
       # f.write( "%6.4f %12.8 %12.8\n" %(np.fft.fftfreq(T)[n], fft[w][1][n][0], fft[w][1][n][1]) )  
-      f.write( "wfs%+.3f     wfs%+.3f     " % (fft[w][0], fft[w][0]) )  
+      f.write( "$t^2{\\\\langle}E{\\\\rangle}=%.2f$     $wfs%+.2f$     " % (fft[w][0], fft[w][0]) )  
   f.write("\n")
   for n in range(T):
     f.write("%+6.4f " % (np.fft.fftfreq(T)[n]))
@@ -243,6 +266,7 @@ def compute_ens_wfs(ens_name, Njack, T, tau_limit, dtau, fit_min, fit_max, label
       # f.write( "%6.4f %12.8 %12.8\n" %(np.fft.fftfreq(T)[n], fft[w][1][n][0], fft[w][1][n][1]) )  
       f.write( "%+10.6e %+10.6e " % (fft[w][1][n][0], fft[w][1][n][1]) )  
     f.write("\n")
+  return num_col
 
 # ens_name, Njack, T, tau_limit, dtau, fit_min, fit_max
 #compute_ens("periodic-12x32x12ID-B", 87, 32, 10, 1, 2, 10, "mdwf1.dat")
@@ -273,10 +297,13 @@ print("T = %d" % ens_t)
 ens_dtau = int(sys.argv[4])
 print("dtau = %d" % ens_dtau)
 
-compute_ens_wfs(ens_stem, njack, ens_t, 5, ens_dtau, 0, 6, "%s_adaptive.dat"%ens_stem)
+num_col = compute_ens_wfs(ens_stem, njack, ens_t, 5, ens_dtau, 0, 6, "%s_adaptive.dat"%ens_stem, False)
+generate_gnuplot_script(ens_stem, ens_dtau, num_col, False)
+os.system("gnuplot -c plots/"+ ens_stem +"_adaptive.gnuplot")
 
-generate_gnuplot_script(ens_stem, ens_dtau)
-os.system("gnuplot -c "+ ens_stem +"_adaptive.gnuplot")
+compute_ens_wfs(ens_stem, njack, ens_t, 5, ens_dtau, 0, 6, "%s_difference.dat"%ens_stem, True)
+generate_gnuplot_script(ens_stem, ens_dtau, num_col, True)
+os.system("gnuplot -c plots/"+ ens_stem +"_difference.gnuplot")
 
 #ens_name="periodic-16x32"
 #Njack = 20
